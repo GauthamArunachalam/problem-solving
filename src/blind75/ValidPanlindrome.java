9508f39dc67731bba144755353229156f2414d9c
Gautham Arunachalam <gautham.arunachalam@fostermoore.com>
2023-08-09 13:58:46 +1200

    1. Valid palindrome

---
+++ src/blind75/ValidPanlindrome.java
@@ -0,0 +1,54 @@
+package blind75;
+
+import java.util.Scanner;
+
+public class ValidPanlindrome {
+
+    public static boolean isAlphaNumeric(char c){
+        if((c >=48 && c <=57) || (c >=65 && c <= 90) || (c >= 97 && c <= 122)){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isPanlindrome(String input){
+        char[] inputValue = input.toCharArray();
+        int i = 0, j=inputValue.length-1;
+
+        while(i<j){
+            if(!isAlphaNumeric(inputValue[i])){
+                i++;
+                continue;
+            }
+            if(!isAlphaNumeric(inputValue[j])){
+                j--;
+                continue;
+            }
+
+            if(inputValue[i] >=65 && inputValue[i]<=90){
+                inputValue[i] = (char)(inputValue[i] + 32);
+            }
+
+            if(inputValue[j] >= 65 && inputValue[j] <= 90){
+                inputValue[j] = (char) (inputValue[j] + 32);
+            }
+
+            if(inputValue[i] != inputValue[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        String input;
+
+        System.out.println("Enter the input string : ");
+        input = scan.nextLine();
+
+        System.out.println(isPanlindrome(input));
+    }
+}
