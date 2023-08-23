62e51d59b4299d03954ff5d47dbde7ad86420cc3
Gautham Arunachalam <gauthamarunachalam1998@gmail.com>
2023-08-23 18:19:04 +1200

    1. Single non repeating number in arr

---
+++ src/leetcode/SingleNumber.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import utils.InputDataReader;
+
+public class SingleNumber {
+
+    public static int singleNumber(int[] nums){
+        int res = nums[0];
+        for(int i=1; i<nums.length; i++){
+            res = res ^ nums[i];
+        }
+        return res;
+    }
+
+    public static void main(String args[]){
+        InputDataReader inputDataReader = new InputDataReader();
+        int nums[] = inputDataReader.readIntArr();
+
+        System.out.println(singleNumber(nums));
+    }
+}
