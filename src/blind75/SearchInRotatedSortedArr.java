f789b150a2512a36da00a51db28306f47d403045
Gautham Arunachalam <gauthamarunachalam1998@gmail.com>
2023-09-10 00:29:46 +1200

    1. search in rotated sorted array - issue fix

--- src/blind75/SearchInRotatedSortedArr.java
+++ src/blind75/SearchInRotatedSortedArr.java
@@ -6,12 +6,12 @@ public class SearchInRotatedSortedArr {
 
     public static int search (int[] nums, int target) {
         int low = 0, high = nums.length - 1;
-        while (low < high) {
+        while (low <= high) {
             int mid = (low + high) / 2;
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[low] <= nums[mid]) {
-                 if ((target > nums[mid]) || (target < nums[high])) {
+                 if ((target > nums[mid]) || (target < nums[low])) {
                      low = mid + 1;
                  } else {
                      high = mid - 1;
